177dfb6f7186e9fcbf34132336165051d75c7702
shahab Ramzan <96526562+shahab-ramzan@users.noreply.github.com>
2022-12-21 21:59:04 +0500

    react router added

--- Pet.jsx
+++ Pet.jsx
@@ -1,9 +1,10 @@
+import { Link } from "react-router-dom";
 const Pet = ({ name, animal, location, breed, images, id }) => {
   let hero = images?.length
     ? images[0]
     : "http://pets-images.dev-apis.com/pets/none.jpg";
   return (
-    <a href={`/details/${id}`} className="pet">
+    <Link to={`/details/${id}`} className="pet">
       <div className="image-container">
         <img src={hero} alt={name} />
       </div>
@@ -13,7 +14,7 @@ const Pet = ({ name, animal, location, breed, images, id }) => {
           {animal} - {breed} - {location}
         </h2>
       </div>
-    </a>
+    </Link>
   );
 };
 export default Pet;
