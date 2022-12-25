cccea1db335266a0b7b75f5d011e84cc3bd072ec
shahab Ramzan <96526562+shahab-ramzan@users.noreply.github.com>
2022-12-25 20:34:24 +0500

    refactoring-fetchBreedList and cached with react query

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,6 +1,6 @@
 import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
-import fetchPet from "./fetchPet";
+import fetchPet from "./fetchPet.js";
 const Details = () => {
   const { id } = useParams();
   const results = useQuery(["details", id], fetchPet);
